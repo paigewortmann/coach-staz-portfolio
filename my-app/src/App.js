a5e6baa4478761a5000f1820162bc8b8e4523d4e
Paige Wortmann <pagewortmann@gmail.com>
2022-01-15 14:59:57 -0500

    progress with submitting email

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import contactForm from './components/contactForm';
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <div className="App">
+        <contactForm></contactForm>
+      </div>
+    </main>
+  );
+}
+
+export default App;
