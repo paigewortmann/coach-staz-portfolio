92a59b3f3740b0979e06169f446a22761e69c79f
Paige Wortmann <pagewortmann@gmail.com>
2022-01-23 14:00:30 -0500

    Home is progressing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css'
 import ContactForm from './components/contactForm';
+import Home from './components/home'
 
 function App() {
   return (
     <main>
       <div className="App">
+          <Home />
           <ContactForm />
+
       </div>
     </main>
   );
---
+++ my-app/src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../componentStyles/homeStyle.css'
+
+function headerBar() {
+    return (
+        <header className='logo-background'>
+            <img src='IKOS.png' alt='IKOS logo' className='logo'/>
+        </header>
+    )
+}
+
+export default headerBar;
+
