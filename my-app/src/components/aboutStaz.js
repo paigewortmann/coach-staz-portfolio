dad1de8bb60252b19bd6d3bfe2c2bb1d05d839ae
Paige <pagewortmann@gmail.com>
2022-02-23 13:06:23 -0500

    progress towards whyLift and about.

--- my-app/src/components/aboutStaz.js
+++ my-app/src/components/aboutStaz.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import '../componentStyles/aboutStazStyle.css'
 
 function aboutStaz() {
-  return(
-    <div>
-        <h1>Why Coach Staz?</h1>
+  return (
+    <section className='section'>
+      <main className='aboutStaz'>
+        <h1 className='title'>Why Coach Staz?</h1>
         <p>Someone who knows that there talking about</p>
-    </div>
-  ) 
+      </main>
+    </section>
+
+  )
 }
 
 export default aboutStaz;
--- my-app/src/components/whyLift.js
+++ my-app/src/components/whyLift.js
@@ -3,10 +3,13 @@ import '../componentStyles/whyLiftStyle.css'
 
 function whyLift() {
   return (
+    <section className='section'>
       <main className='whyLift'>
-          <h1 className='title'>Why Lift?</h1>
-          <p className='desc='>Because I said so bla bla bla. Bullet points could go here, too. Idk</p> 
+        <h1 className='title'>Why Lift?</h1>
+        <p className='desc'>Because I said so bla bla bla. Bullet points could go here, too. Idk</p>
       </main>
+    </section>
+
   )
 }
 
